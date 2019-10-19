268c1dc9a3c2ea2c7e8a29fd21d313b7834747b4
Mario <a@a.com>
2019-10-19 15:40:50 +0100

    Eloquent relationships

--- app/Project.php
+++ app/Project.php
@@ -8,7 +8,14 @@ class Project extends Model
 {
 //    protected $guarded = []; - if you know what your are doing you can use 'guarded' is oposite of 'fillable'
 // use guarded function only if you are sure that nobody will do: Project::crate(request()->all()); - otherwise somebody can malicously edit form and add hidden input to compromise data
-    protected $fillable = [
+/*    protected $fillable = [
         'title', 'description'
     ];
+*/
+    protected $guarded = [];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,8 +1,17 @@
 @extends ('layout')
 @section ('content')
     <h1 class="title">{{ $project->title }}</h1>
-    <div class="content">{{ $project->description }}</div>
-    <p>
-        <a href="/projects/{{ $project->id }}/edit">Edit</a>
-    </p>
+    <div class="content">
+        {{ $project->description }}
+        <p>
+            <a href="/projects/{{ $project->id }}/edit">Edit</a>
+        </p>
+    </div>
+    @if ($project->tasks->count())
+        <div>
+        @foreach($project->tasks as $task)
+            <li>{{ $task->description }}</li>
+        @endforeach
+        </div>
+    @endif
 @endsection
